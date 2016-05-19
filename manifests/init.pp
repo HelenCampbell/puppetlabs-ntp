b17ed4ea1c1f8b7b0b8d4668df6b5988af4a8ff6
David Schmitt <david.schmitt@puppet.com>
2016-05-19 14:26:33 +0100

    Change validation to use data type annotations
    This requires more work to split out the local aliases to stdlib, encode
    the missing validations, and add depreciations to the current hodge-podge,
    to tighten down what is allowed. Many of the unchecked edge-cases will
    result in a broken ntp config anyways, but just fixing it would elevate
    issues in this area from broken ntp service to broken catalog.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,91 +1,66 @@
+# these should go to stdlib
+type Ntp::Is_absolute_path = Variant[Tea::Absolutepath_compat, Array[Tea::Absolutepath_compat]]
+type Ntp::Is_numeric_string = Pattern[/^-?(?:(?:[1-9]\d*)|0)(?:\.\d+)?(?:[eE]-?\d+)?$/]
+type Ntp::Is_numeric = Variant[Numeric, Ntp::Is_numeric_string]
+
 class ntp (
-  $autoupdate        = $ntp::params::autoupdate,
-  $broadcastclient   = $ntp::params::broadcastclient,
-  $config            = $ntp::params::config,
-  $config_dir        = $ntp::params::config_dir,
-  $config_file_mode  = $ntp::params::config_file_mode,
-  $config_template   = $ntp::params::config_template,
-  $disable_auth      = $ntp::params::disable_auth,
-  $disable_dhclient  = $ntp::params::disable_dhclient,
-  $disable_kernel    = $ntp::params::disable_kernel,
-  $disable_monitor   = $ntp::params::disable_monitor,
-  $fudge             = $ntp::params::fudge,
-  $driftfile         = $ntp::params::driftfile,
-  $leapfile          = $ntp::params::leapfile,
-  $logfile           = $ntp::params::logfile,
-  $iburst_enable     = $ntp::params::iburst_enable,
-  $keys              = $ntp::params::keys,
-  $keys_enable       = $ntp::params::keys_enable,
-  $keys_file         = $ntp::params::keys_file,
-  $keys_controlkey   = $ntp::params::keys_controlkey,
-  $keys_requestkey   = $ntp::params::keys_requestkey,
-  $keys_trusted      = $ntp::params::keys_trusted,
-  $minpoll           = $ntp::params::minpoll,
-  $maxpoll           = $ntp::params::maxpoll,
-  $package_ensure    = $ntp::params::package_ensure,
-  $package_manage    = $ntp::params::package_manage,
-  $package_name      = $ntp::params::package_name,
-  $panic             = $ntp::params::panic,
-  $peers             = $ntp::params::peers,
-  $preferred_servers = $ntp::params::preferred_servers,
-  $restrict          = $ntp::params::restrict,
-  $interfaces        = $ntp::params::interfaces,
-  $interfaces_ignore = $ntp::params::interfaces_ignore,
-  $servers           = $ntp::params::servers,
-  $service_enable    = $ntp::params::service_enable,
-  $service_ensure    = $ntp::params::service_ensure,
-  $service_manage    = $ntp::params::service_manage,
-  $service_name      = $ntp::params::service_name,
-  $service_provider  = $ntp::params::service_provider,
-  $stepout           = $ntp::params::stepout,
-  $tinker            = $ntp::params::tinker,
-  $tos               = $ntp::params::tos,
-  $tos_minclock      = $ntp::params::tos_minclock,
-  $tos_minsane       = $ntp::params::tos_minsane,
-  $tos_floor         = $ntp::params::tos_floor,
-  $tos_ceiling       = $ntp::params::tos_ceiling,
-  $tos_cohort        = $ntp::params::tos_cohort,
-  $udlc              = $ntp::params::udlc,
-  $udlc_stratum      = $ntp::params::udlc_stratum,
-  $ntpsigndsocket    = $ntp::params::ntpsigndsocket,
-  $authprov          = $ntp::params::authprov,
+  Boolean $autoupdate,
+  $broadcastclient,
+  Ntp::Is_absolute_path $config,
+  Optional[Any] $config_dir = undef,
+  $config_file_mode,
+  String $config_template,
+  Boolean $disable_auth,
+  Boolean $disable_dhclient,
+  Boolean $disable_kernel,
+  Boolean $disable_monitor,
+  Array $fudge,
+  Ntp::Is_absolute_path $driftfile,
+  Optional[Ntp::Is_absolute_path] $leapfile = undef,
+  Optional[Ntp::Is_absolute_path] $logfile = undef,
+  Boolean $iburst_enable,
+  Array[Any] $keys,
+  Boolean $keys_enable,
+  $keys_file,
+  Variant[Integer, Pattern[/^\d+$/, //]] $keys_controlkey,
+  Variant[Integer, Pattern[/^\d+$/, //]] $keys_requestkey,
+  Array[Any] $keys_trusted,
+  Optional[Variant[Integer[3, 16], Float[3, 16], Ntp::Is_numeric_string]] $minpoll = undef,
+  Optional[Variant[Integer[3, 16], Float[3, 16], Ntp::Is_numeric_string]] $maxpoll = undef,
+  String $package_ensure,
+  Boolean $package_manage,
+  Array[Any] $package_name,
+  Optional[Variant[Integer[0, 65535], Ntp::Is_numeric_string]] $panic = $::ntp::params::panic,
+  $peers,
+  Array[Any] $preferred_servers,
+  Array[Any] $restrict,
+  Array[Any] $interfaces,
+  $interfaces_ignore,
+  Array[Any] $servers,
+  Boolean $service_enable,
+  String $service_ensure,
+  $service_manage,
+  $service_name,
+  Optional[Any] $service_provider = undef,
+  $stepout = undef,
+  Boolean $tinker = $::ntp::params::tinker,
+  Boolean $tos,
+  Optional[Ntp::Is_numeric] $tos_minclock,
+  $tos_minsane,
+  $tos_floor,
+  $tos_ceiling,
+  $tos_cohort,
+  $udlc,
+  $udlc_stratum,
+  Optional[Ntp::Is_absolute_path] $ntpsigndsocket = undef,
+  Optional[String] $authprov = undef,
 ) inherits ntp::params {
 
-  validate_bool($broadcastclient)
-  validate_absolute_path($config)
-  validate_string($config_template)
-  validate_bool($disable_auth)
-  validate_bool($disable_dhclient)
-  validate_bool($disable_kernel)
-  validate_bool($disable_monitor)
-  validate_absolute_path($driftfile)
-  if $logfile { validate_absolute_path($logfile) }
-  if $ntpsigndsocket { validate_absolute_path($ntpsigndsocket) }
-  if $leapfile { validate_absolute_path($leapfile) }
-  validate_bool($iburst_enable)
-  validate_array($keys)
-  validate_bool($keys_enable)
-  validate_re($keys_controlkey, ['^\d+$', ''])
-  validate_re($keys_requestkey, ['^\d+$', ''])
-  validate_array($keys_trusted)
   if $minpoll { validate_numeric($minpoll, 16, 3) }
   if $maxpoll { validate_numeric($maxpoll, 16, 3) }
-  validate_string($package_ensure)
-  validate_bool($package_manage)
-  validate_array($package_name)
   if $panic { validate_numeric($panic, 65535, 0) }
-  validate_array($preferred_servers)
-  validate_array($restrict)
-  validate_array($interfaces)
-  validate_array($servers)
-  validate_array($fudge)
-  validate_bool($service_enable)
-  validate_string($service_ensure)
-  validate_bool($service_manage)
-  validate_string($service_name)
   if $stepout { validate_numeric($stepout, 65535, 0) }
-  validate_bool($tinker)
-  validate_bool($tos)
+
   if $tos_minclock { validate_numeric($tos_minclock) }
   if $tos_minsane { validate_numeric($tos_minsane) }
   if $tos_floor { validate_numeric($tos_floor) }
